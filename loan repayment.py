607329739994d91d460f799c8ab63a1c5a53ff2e
lizechon <136400204+lizechon@users.noreply.github.com>
2023-06-16 01:12:38 -0700

    Update loan repayment.py

--- loan repayment.py
+++ loan repayment.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun 14 21:02:02 2023
+Created on Wed Jun 14 21:02:02 2023
 
 @author: echon
 """
@@ -12,7 +12,7 @@ def PMT(PV, r, n):
     return loan_payment
 
 # Inputs
-PV = float(input("Enter the Present Value:$"))      # PresentValue
+PV = float(input("Enter the Present Value:$"))      # Present Value
 r = float(input("Enter interest rate %: "))         # APR 
 n = int(input("Enter the number of Months:"))       # Months  
 
